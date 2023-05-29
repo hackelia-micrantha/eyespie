a4135213b255715bbdfc5d41b5dbb57dac7a0465
Ryan Jennings <rybertjen@gmail.com>
2023-05-28 22:04:55 -0700

    fix(flux): pass screen scope to store to prevent lingering effects

--- shared/src/commonMain/kotlin/com/micrantha/bluebell/domain/flux/Flux.kt
+++ shared/src/commonMain/kotlin/com/micrantha/bluebell/domain/flux/Flux.kt
@@ -3,11 +3,12 @@ package com.micrantha.bluebell.domain.flux
 import com.micrantha.bluebell.domain.arch.Dispatcher
 import com.micrantha.bluebell.domain.arch.Store
 import com.micrantha.bluebell.domain.arch.StoreFactory
+import kotlinx.coroutines.CoroutineScope
 
 class Flux(
     private val dispatcher: FluxDispatcher
 ) : StoreFactory, Dispatcher by dispatcher {
 
-    override fun <T> createStore(state: T): Store<T> =
-        FluxStore(state).apply { dispatcher.register(this::dispatch) }
+    override fun <T> createStore(initialState: T, scope: CoroutineScope): Store<T> =
+        FluxStore(initialState, scope).apply { dispatcher.register(this::dispatch) }
 }
