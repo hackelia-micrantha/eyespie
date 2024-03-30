065949c73d8f53f33f3b2774e5e333557fa48b45
Ryan Jennings <ryan.jennings@micrantha.com>
2024-03-30 01:12:33 -0700

    chore: rename app folders

--- android/app/src/main/java/com/eyespie/MainApplication.kt
+++ android/app/src/main/java/com/eyespie/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.eyes_pie
+package com.eyespie
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/eyespieTests/eyes_pieTests.m
+++ ios/eyespieTests/eyes_pieTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface eyes_pieTests : XCTestCase
+@interface eyespieTests : XCTestCase
 
 @end
 
-@implementation eyes_pieTests
+@implementation eyespieTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
