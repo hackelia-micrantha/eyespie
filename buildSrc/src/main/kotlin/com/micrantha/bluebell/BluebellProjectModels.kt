2f7aca2d6c801094cadfd06322d8847ddd0e5c39
Ryan Jennings <rybertjen@gmail.com>
2024-12-21 14:57:56 -0800

    chore: upgrade to kotlin 2.1.0 and dependencies

--- buildSrc/src/main/kotlin/com/micrantha/bluebell/BluebellProjectModels.kt
+++ buildSrc/src/main/kotlin/com/micrantha/bluebell/BluebellProjectModels.kt
@@ -9,8 +9,6 @@ fun Project.configureModels(models: BluebellModels) {
         group = "Bluebell"
         description = "Configure machine learning models"
 
-        downloadModels(models)
-
         if (models.outputPaths.isNotEmpty()) {
             copyModels(models)
         }
@@ -19,24 +17,12 @@ fun Project.configureModels(models: BluebellModels) {
     tasks.findByName("generateBluebellConfig")?.dependsOn(task)
 }
 
-internal fun downloadModels(models: BluebellModels) {
-    /*execute("git", "lfs", "install")
-
-    for (id in models.ids) {
-        println("Fetching model: $id")
-        System.out.flush()
-        execute(arrayOf("git", "clone", "https://huggingface.co/$id"), outputDirectory)
-    }
-    println("${models.ids.size} machine learning models retrieved.")
-     */
-}
-
 internal fun copyModels(models: BluebellModels) {
 
     println("> Model output paths:")
 
     for (outputPath in models.outputPaths) {
-        println("  - ${outputPath}")
+        println("  - $outputPath")
         File(outputPath).mkdirs()
     }
 
@@ -53,19 +39,5 @@ internal fun copyModels(models: BluebellModels) {
             val to = File(outputPath, entry.value)
             from.copyTo(to, true)
         }
-    }
-}
-
-private fun execute(vararg command: String) = execute(command = command, outputDirectory = null)
-
-private fun execute(command: Array<out String>, outputDirectory: File? = null) {
-    try {
-        val process = ProcessBuilder(*command).apply {
-            outputDirectory?.let { directory(it) }
-        }.start()
-
-        process.waitFor()
-    } catch (err: Throwable) {
-        err.printStackTrace()
     }
 }
