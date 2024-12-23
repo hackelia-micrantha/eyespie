95d846240fc8727b36d5370d4de6d9b5f8ff711f
Ryan Jennings <rybertjen@gmail.com>
2024-12-22 19:51:10 -0800

    fix: remove optional app config delegate due to type error

--- composeApp/src/commonMain/kotlin/com/micrantha/eyespie/AppConfig.kt
+++ composeApp/src/commonMain/kotlin/com/micrantha/eyespie/AppConfig.kt
@@ -1,13 +1,12 @@
 package com.micrantha.eyespie
 
 import com.micrantha.bluebell.platform.AppConfigDelegate
-import com.micrantha.bluebell.platform.OptionalAppConfigDelegate
-import com.micrantha.eyespie.config.LOGIN_EMAIL
-import com.micrantha.eyespie.config.LOGIN_PASSWORD
+import com.micrantha.eyespie.config.LOGIN_EMAIL as DEBUG_LOGIN_EMAIL
+import com.micrantha.eyespie.config.LOGIN_PASSWORD as DEBUG_LOGIN_PASSWORD
 
 object AppConfig {
-    val LOGIN_EMAIL by OptionalAppConfigDelegate(LOGIN_EMAIL)
-    val LOGIN_PASSWORD by OptionalAppConfigDelegate(LOGIN_PASSWORD)
+    val LOGIN_EMAIL = DEBUG_LOGIN_EMAIL
+    val LOGIN_PASSWORD = DEBUG_LOGIN_PASSWORD
     val SUPABASE_URL by AppConfigDelegate
     val SUPABASE_KEY by AppConfigDelegate
     val HUGGING_FACE_TOKEN by AppConfigDelegate
