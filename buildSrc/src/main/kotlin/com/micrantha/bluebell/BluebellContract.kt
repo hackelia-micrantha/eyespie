5b362d20a278a7db4244ab93ce6bd57fc32229af
Ryan Jennings <rybertjen@gmail.com>
2025-05-25 22:46:58 -0700

    build: add default graphql endpoint

--- buildSrc/src/main/kotlin/com/micrantha/bluebell/BluebellContract.kt
+++ buildSrc/src/main/kotlin/com/micrantha/bluebell/BluebellContract.kt
@@ -16,7 +16,7 @@ open class BluebellConfig {
 open class GraphqlConfig {
     var serviceName: String = ""
     var packagePath: String? = null
-    var endpoint: String? = null
+    var endpoint: String = "http://localhost:8080"
     var headers: Map<String, String> = emptyMap()
 }
 
